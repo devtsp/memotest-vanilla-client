4348f979c93dcbd942d91bc4bd106cc96a3918cb
Tomas <87679143+devtsp@users.noreply.github.com>
2022-01-13 21:51:39 -0300

    First Commit
    At the moment  ul#memotest displays  the required duplicated images on each reload at random.

---
+++ main.js
@@ -0,0 +1,51 @@
+const $memoTest = document.querySelector('#memotest');
+const memoTestCards = Array.from($memoTest.children);
+
+function memoTestRandomDisplay() {
+	const imageEntries = {};
+
+	function imageRandomizer() {
+		const imageReferences = [
+			'335.jfif',
+			'explorer.jfif',
+			'iceman.jfif',
+			'jaguar.jfif',
+			'lp.jfif',
+			'sg.jfif',
+			'strat.jfif',
+			'tele.jfif',
+		];
+		const randomNumber = Math.floor(Math.random() * 8);
+		const randomImage = imageReferences[randomNumber];
+		return randomImage;
+	}
+
+	function isDuplicated(randomImage) {
+		if (imageEntries[randomImage] === 2) {
+			return true;
+		} else if (imageEntries[randomImage] === 1) {
+			imageEntries[randomImage]++;
+			return false;
+		} else {
+			imageEntries[randomImage] = 1;
+			return false;
+		}
+	}
+
+	function imageAssigner(card) {
+		const selectImg = imageRandomizer();
+		const duplicated = isDuplicated(selectImg);
+		if (duplicated) {
+			imageAssigner(card)
+    } else {
+      card.name = selectImg.match(/^[\w\d-_]+/);
+			card.style.backgroundImage = `url("./img/${selectImg}")`;
+    }
+	}
+
+	memoTestCards.forEach(card => {
+		imageAssigner(card)
+	});
+}
+
+memoTestRandomDisplay();
