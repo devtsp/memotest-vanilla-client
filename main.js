a17d4e15ebdb52b3c273f2406076b1d7a0354061
Tomas <87679143+devtsp@users.noreply.github.com>
2022-01-18 02:24:28 -0300

    Refactor start
    Game state now is a new instance of FreshGame class, all DOM queries will be gloablly available, the shuffler helpers now are pure functions (they also can handle new images if the content of img/theme directory was changed)

--- main.js
+++ main.js
@@ -1,232 +1,61 @@
-window.addEventListener('DOMContentLoaded', () => {
-	initialState();
-});
-
-function initialState() {
-	const clickCounter = { clicks: 0 };
-	const cardsSelected = [];
-	const selectionsIDs = [];
-	const timerCount = { seconds: 0 };
-	const triesCount = { tries: 0 };
-	const totalMatches = { matches: 0 };
-	document.querySelector('#shuffle').onclick = () => {
-		location.reload();
-	};
-	document.querySelector('#win').classList.add('none');
-	document.querySelector('#progress').classList.remove('none');
-
-	startStats(timerCount, triesCount, totalMatches);
-	backSidesShuffler();
-	frontSidesShuffler(
-		clickCounter,
-		cardsSelected,
-		totalMatches,
-		selectionsIDs,
-		triesCount,
-		timerCount
-	);
+class FreshGame {
+	playing = true;
+	clickCounter = 0;
+	selectionsNames = [];
+	selectionsIDs = [];
+	timer = 0;
+	attempts = 0;
+	coincidences = 0;
 }
 
-function backSidesShuffler() {
-	let id = 0;
-	const imagePastEntries = {};
-	const $cardBacks = document.querySelectorAll('#memotest li');
-	$cardBacks.forEach(back => {
-		imageAssigner(back, imagePastEntries);
-		back.classList.remove('hidden');
-		back.style.backgroundColor = 'white';
-		back.id = ++id;
-		back.tabIndex = 0;
-	});
-}
-
-function frontSidesShuffler(
-	clickCounter,
-	cardsSelected,
-	totalMatches,
-	selectionsIDs,
-	triesCount,
-	timerCount
-) {
-	const $cardFronts = document.querySelectorAll('#memotest img');
-	$cardFronts.forEach(card => {
-		card.classList.remove('hidden');
-		card.src = './img/questionmark.jpg';
-		card.addEventListener('click', e =>
-			handleClick(
-				e,
-				clickCounter,
-				cardsSelected,
-				totalMatches,
-				selectionsIDs,
-				triesCount,
-				timerCount
-			)
-		);
-	});
-}
-
-function startStats(timerCount, triesCount, totalMatches) {
-	// Awfull solution to clear past intervals. I tried EVERYTHING
-	for (let i = 0; i < 5; i++) {
-		clearInterval(i);
-	}
-	const $tries = document.querySelector('#tries');
-	const $timer = document.querySelector('#timer');
-	$tries.innerText = `${triesCount.tries}`;
-	$timer.innerText = `${timerCount.seconds}`;
-	const intervalID = setInterval(() => {
-		timerCount.seconds++;
-		$timer.innerText = `${timerCount.seconds / 10}`;
-		if (totalMatches.matches === 8) {
-			clearInterval(intervalID);
-		}
-	}, 100);
-}
-
-// SHUFFLE PROCEDURAL FUNCTION:
-function imageAssigner(back, imagePastEntries) {
-	const randomBackImg = imageRandomizer();
-	const duplicated = isDuplicated(randomBackImg, imagePastEntries);
-	if (duplicated) {
-		imageAssigner(back, imagePastEntries);
-	} else {
-		back.setAttribute('name', randomBackImg.match(/^[\w\d-_]+/));
-		back.style.backgroundImage = `url("./img/guitars/${randomBackImg}")`;
-	}
-}
-
-function imageRandomizer() {
-	const imageReferences = [
-		'335.jfif',
-		'explorer.jfif',
-		'iceman.jfif',
-		'jaguar.jfif',
-		'lp.jfif',
-		'sg.jfif',
-		'strat.jfif',
-		'tele.jfif',
-	];
-	const randomNumber = Math.floor(Math.random() * 8);
-	const randomImage = imageReferences[randomNumber];
-	return randomImage;
-}
-
-function isDuplicated(currentEntry, pastEntries) {
-	if (pastEntries[currentEntry] === 2) {
-		return true;
-	} else if (pastEntries[currentEntry] === 1) {
-		pastEntries[currentEntry]++;
-		return false;
-	} else {
-		pastEntries[currentEntry] = 1;
-		return false;
-	}
-}
-
-// USER INPUT PROCEDURAL
-function handleClick(
-	e,
-	clickCounter,
-	cardsSelected,
-	totalMatches,
-	selectionsIDs,
-	triesCount,
-	timerCount
-) {
-	changeState(e, clickCounter, cardsSelected, selectionsIDs);
-	if (clickCounter.clicks === 2) {
-		triesCount.tries++;
-		document.querySelector('#tries').innerText = `${triesCount.tries}`;
-		clickDisabler();
-		matchOrMismatch(clickCounter, cardsSelected, selectionsIDs, totalMatches);
-	}
-	checkWin(totalMatches, triesCount, timerCount);
-}
-
-function changeState(e, clickCounter, cardsSelected, selectionsIDs) {
-	e.target.classList.add('hidden');
-	clickCounter.clicks++;
-	selectionsIDs.push(e.target.parentNode.id);
-	const cardName = e.target.parentNode.getAttribute('name');
-	cardsSelected.push(cardName);
-}
-
-function clickDisabler() {
-	document.body.style.pointerEvents = 'none';
-	setTimeout(() => {
-		document.body.style.pointerEvents = 'all';
-	}, 300);
-}
-
-function matchOrMismatch(
-	clickCounter,
-	cardsSelected,
-	selectionsIDs,
-	totalMatches
-) {
-	if (
-		cardsSelected[0] === cardsSelected[1] &&
-		selectionsIDs[0] !== selectionsIDs[1]
-	) {
-		handleMatch(cardsSelected, totalMatches, selectionsIDs);
-	} else {
-		handleMismatch(cardsSelected, selectionsIDs);
-	}
-	clickCounter.clicks = 0;
-}
-
-function handleMatch(cardsSelected, totalMatches, selectionsIDs) {
-	totalMatches.matches++;
-	const $matchedPair = document.querySelectorAll(
-		`[name="${cardsSelected[0]}"]`
-	);
-	const $progress = document.querySelector('#matches');
-	const percentage = (totalMatches.matches * 100) / 8;
-	$progress.ariaValueNow = `${percentage}%`;
-	$progress.style.width = `${percentage}%`;
-	setTimeout(() => {
-		$matchedPair.forEach(matchedCard => {
-			matchedCard.classList.add('hidden');
-    });
-    for (; selectionsIDs.length > 0; ) {
-      selectionsIDs.pop();
+const $restartGame = document.querySelector('#restart-game'),
+  $timer = document.querySelector('#timer'),
+  $attempts = document.querySelector('#attempts'),
+  $winMessage = document.querySelector('#win-message'),
+  $backSides = document.querySelectorAll('#memotest li'),
+  $frontSides = document.querySelectorAll('#memotest li img'),
+  $progressBar = document.querySelector('#progress-bar'),
+  imageReferences = [
+    '335.jfif',
+    'explorer.jfif',
+    'iceman.jfif',
+    'jaguar.jfif',
+    'lp.jfif',
+    'sg.jfif',
+    'strat.jfif',
+    'tele.jfif',
+  ],
+  duplicateArray = array => array.concat(array),
+  randomizeArray = array => array.sort(() => Math.random() - Math.random()),
+  newDuplicateRandomArray = array => randomizeArray(duplicateArray(array));
+	
+const shuffleCards = (slots, content) => {
+  slots.forEach(
+    slot => {
+      let item = content.pop();
+      slot.style.backgroundImage = `url("./img/theme/${item}")`;
+      slot.name = item.match(/^[\w\d-_]+/)
     }
-    for (; cardsSelected.length > 0; ) {
-			cardsSelected.pop();
-    }
-	}, 300);
+  )
 }
 
-function handleMismatch(cardsSelected, selectionsIDs) {
-	const $cards = document.querySelectorAll(`#memotest li`);
-	setTimeout(() => {
-		$cards.forEach(card => {
-			if (selectionsIDs.includes(card.id)) {
-				card.children[0].classList.remove('hidden');
-			}
-		});
-		for (; selectionsIDs.length > 0; ) {
-			selectionsIDs.pop();
-    }
-    for (; cardsSelected.length > 0; ) {
-			cardsSelected.pop();
-		}
-	}, 300);
-}
+renderTimer = slot => {
+  for (let i = 0; i < 999; i++) {
+    clearInterval(i);
+  }
+  let timer = 0;
+  return setInterval(() => {
+    timer++;
+    slot.innerText = `${timer / 10}`;
+  }, 100);
+};
 
-function checkWin(totalMatches, triesCount, timerCount) {
-	const $message = document.querySelector('#win');
-	if (totalMatches.matches === 8) {
-		setTimeout(() => {
-			$message.classList.remove('none');
-			document.querySelector('#progress').classList.add('none');
-			$message.querySelector(
-				'#wintries'
-			).innerText = `${triesCount.tries} tries`;
-			$message.querySelector(
-				'#winseconds'
-			).innerText = `${timerCount.seconds / 10} seconds`;
-		}, 300);
-	}
-}
+$restartGame.onclick = () => {
+	startGame = new FreshGame();
+	shuffleCards($backSides, newDuplicateRandomArray(imageReferences));
+	renderTimer($timer);
+};
+
+
+
+
